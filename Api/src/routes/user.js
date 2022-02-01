50c125def93d8c7c72bd03c366d8c29e25a58c09
AndresLoaiza <andres260382@gmail.com>
2022-02-01 17:37:03 -0500

    feature landing

---
+++ Api/src/routes/user.js
@@ -0,0 +1,62 @@
+const { Router } = require('express');
+const router = Router();
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+//intentando subir rutas.
+
+const { User } = require('../db');
+
+router.post('/', async (req, res) => {
+    const { name, dni, email, password, centroName, rol } = req.body;
+
+    try {
+        const newUser = {
+            name,
+            dni,
+            email,
+            password,
+            centroName,
+            rol,
+        };
+
+        console.log('req -> ' + JSON.stringify(req.body));
+        await User.create(newUser);
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'ep3977752@gmail.com',
+                pass: 'ekcocqalmsequlhp',
+            },
+        });
+
+        const mailOptions = {
+            from: 'Remitente',
+            to: email,
+            subject: 'Felicitaciones te has registrado con exito',
+            text: `¡Gracias ${name} por registrarte en nustra web!`,
+        };
+
+        await transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Email enviado');
+            }
+        });
+
+        return res.json({ message: 'new user created!', user: newUser });
+    } catch (err) {
+        return console.log('este es el error', err)
+        
+        // res.json({
+        //     message: 'user already exists!',
+        // });
+    }
+});
+
+
+
+module.exports = router;
